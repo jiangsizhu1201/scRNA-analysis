e58021e9bd6633146e1872e9bb2222d64a9c85f3
EvelynJ <50031451+jiangsizhu1201@users.noreply.github.com>
2024-05-04 10:56:34 -0400

    Update run-monocle-for-fluidigm.R

--- run-monocle-for-fluidigm.R
+++ run-monocle-for-fluidigm.R
@@ -1,4 +1,4 @@
-###### step1: 获取单细胞转录组数据集 ######
+###### step1 ######
 
 suppressMessages(library(scater))
 suppressMessages(library(scRNAseq))
@@ -32,10 +32,7 @@ fd <- new("AnnotatedDataFrame",
 ct[1:4,1:4]
 
 
-###### step2: 构建monocle的对象，并且进行基础的降维聚类分群 ######
-
-# 构建对象，seurat，monocle，scater 都类似
-# monocle标准流程，降维聚类分群 
+###### step2: monocle ######
 
 sc_cds <- newCellDataSet(
   as.matrix(ct), 
@@ -45,14 +42,11 @@ sc_cds <- newCellDataSet(
   lowerDetectionLimit=1)
 sc_cds
 
-# 这个 CellDataSet 对象一定要认识清楚，务必花两个小时去摸索它。
+# CellDataSet 
 
-
-# 接下来仅仅是  monocle的标准流程而已
 library(monocle)
 sc_cds
 sc_cds <- detectGenes(sc_cds, min_expr = 1) 
-# 数值可以自行摸索
 sc_cds <- sc_cds[fData(sc_cds)$num_cells_expressed > 10, ]
 sc_cds
 
@@ -61,7 +55,7 @@ cds <- estimateSizeFactors(cds)
 cds <- estimateDispersions(cds) 
 # plyr 
 
-# 并不是所有的基因都有作用，所以先进行挑选，合适的基因用来进行聚类。
+# select genes
 disp_table <- dispersionTable(cds)
 unsup_clustering_genes <- subset(disp_table,
                                  mean_expression >= 0.1)
@@ -71,7 +65,7 @@ cds <- setOrderingFilter(cds,
 plot_ordering_genes(cds) 
 plot_pc_variance_explained(cds, 
                            return_all = F) # norm_method='log'
-# 其中 num_dim 参数选择基于上面的PCA图
+# num_dim determined by PCA
 cds <- reduceDimension(cds, max_components = 2, num_dim = 6,
                        reduction_method = 'tSNE', verbose = T)
 cds <- clusterCells(cds, num_clusters = 6) 
@@ -82,27 +76,20 @@ table(pData(cds)$Biological_Condition)
 table(pData(cds)$Cluster,pData(cds)$Biological_Condition) 
 plot_cell_clusters(cds, 1, 2 )
 
-# 可以看到 monocle 给细胞重新定义了亚群，亚群数量是自己选择的
-# 整体来说，monocle和seurat 各自独立流程定义的亚群的一致性还不错。
-
-# 只是跑流程而已
 save(cds,file = 'input_cds.Rdata')
 
 
 
-###### step3: 使用monocle的拟时序分析######
+###### step3: trajectory ######
 
-### 然后查看monocle ### 
+### monocle ### 
 cds 
-# 接下来很重要，到底是看哪个性状的轨迹
+
 table(pData(cds)$Cluster)
 table(pData(cds)$Cluster,pData(cds)$Biological_Condition)
 plot_cell_clusters(cds, 1, 2 )
 
-## 我们这里并不能使用 monocle的分群
-# 还是依据前面的 seurat分群, 也就是说前面的代码仅仅是流程而已，我们没有使用那些结果哦
-
-# 其实取决于自己真实的生物学意图
+# Biological conditions
 pData(cds)$Cluster=pData(cds)$Biological_Condition
 table(pData(cds)$Cluster)
 
@@ -116,7 +103,7 @@ sig_genes <- subset(diff_test_res, qval < 0.1)
 sig_genes=sig_genes[order(sig_genes$pval),]
 head(sig_genes[,c("gene_short_name", "pval", "qval")] ) 
 cg=as.character(head(sig_genes$gene_short_name)) 
-#  挑选差异最显著的基因可视化
+
 plot_genes_jitter(cds[cg,],
                   grouping = "Cluster",
                   color_by = "Cluster",
@@ -129,21 +116,20 @@ plot_genes_jitter(cds[cg2,],
                   nrow= 3,
                   ncol = NULL )
 
-# 前面是找差异基因，后面是做拟时序分析
 
-# 第一步: 挑选合适的基因. 有多个方法，例如提供已知的基因集，
-# 这里选取统计学显著的差异基因列表
+
+# select gene set (significant)
+
 ordering_genes <- row.names (subset(diff_test_res, qval < 0.01))
 ordering_genes
 cds <- setOrderingFilter(cds, ordering_genes)
 plot_ordering_genes(cds) 
-# 第二步: 降维。降维的目的是为了更好的展示数据。函数里提供了很多种方法,
-# 不同方法的最后展示的图都不太一样, 其中“DDRTree”是Monocle2使用的默认方法
+
 cds <- reduceDimension(cds, max_components = 2,
                        method = 'DDRTree')
-# 第三步: 对细胞进行排序
+
 cds <- orderCells(cds)
-# 最后两个可视化函数，对结果进行可视化
+
 plot_cell_trajectory(cds, color_by = "Cluster")  
 ggsave('monocle_cell_trajectory_for_seurat.pdf')
 
@@ -156,24 +142,22 @@ phe=pData(cds)
 boxplot(phe$Pseudotime,phe$Cluster)
 
 # https://davetang.org/muse/2017/10/01/getting-started-monocle/
-# 前面根据差异基因，推断好了拟时序，也就是说把差异基因动态化了
 
-# 后面就可以具体推断哪些基因随着拟时序如何的变化
 my_cds_subset=cds
 # pseudotime is now a column in the phenotypic data as well as the cell state
 head(pData(my_cds_subset))
-# 这个differentialGeneTest会比较耗费时间
+# differentialGeneTest - time consuming
 my_pseudotime_de <- differentialGeneTest(my_cds_subset,
                                          fullModelFormulaStr = "~sm.ns(Pseudotime)",
                                          cores = 1 )
-# 不知道为什么在Mac电脑无法开启并行计算了 ，不过我测试了在Windows 电脑设置cores = 4是可以的
-# 如果你是Mac电脑，自己修改 cores = 1 即可 
+# cluster needed : cores = 4
+# if mac: cores = 1 
 head(my_pseudotime_de)
 save( my_cds_subset,my_pseudotime_de,
       file = 'output_of_monocle.Rdata')
 
 
-###### step4: 可视化monocle的拟时序分析######
+###### step4 ######
 
 library(Seurat)
 library(gplots)
@@ -230,7 +214,7 @@ colnames(pData(my_cds_subset))
 table(pData(my_cds_subset)$Cluster,pData(my_cds_subset)$State) 
 ac=pData(my_cds_subset)[c('Biological_Condition','State','Pseudotime')]
 head(ac)
-# 这个热图绘制的并不是纯粹的细胞基因表达量矩阵，而是被 Pseudotime 好了的100列，50行的矩阵
+# Pseudotime: 100col x 50 row
 
 my_pseudotime_cluster <- plot_pseudotime_heatmap(my_cds_subset[gene_to_cluster,],
                                                  # num_clusters = 2, 
