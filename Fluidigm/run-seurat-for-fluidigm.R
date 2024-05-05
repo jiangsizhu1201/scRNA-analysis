6602d9a37be161a98c3440192479056b2530905c
EvelynJ <50031451+jiangsizhu1201@users.noreply.github.com>
2024-05-05 15:13:24 -0400

    Update and rename run-seurat-for-fluidigm.R to Fluidigm/run-seurat-for-fluidigm.R

--- Fluidigm/run-seurat-for-fluidigm.R
+++ Fluidigm/run-seurat-for-fluidigm.R
@@ -1,5 +1,3 @@
-###### step1 ######
-
 suppressMessages(library(scater))
 suppressMessages(library(scRNAseq))
 library(ggplot2)
@@ -10,6 +8,9 @@ library("factoextra")
 library("ROCR")
 
 ## ----- Load Example Data -----
+
+###### step1 ######
+
 fluidigm <- ReprocessedFluidigmData()
 fluidigm
 
@@ -21,7 +22,7 @@ DT::datatable(sample_ann)
 box <- lapply(colnames(sample_ann[,1:19]),function(i) {
   dat <-  sample_ann[,i,drop=F] 
   dat$sample=rownames(dat)
-  ## 画boxplot 
+  ## boxplot 
   ggplot(dat, aes('all cells', get(i))) +
     geom_boxplot() +
     xlab(NULL)+ylab(i)
@@ -30,50 +31,41 @@ plot_grid(plotlist=box, ncol=5 )
 
 library(Seurat)
 sce.all=CreateSeuratObject(counts = ct)
-sce.all 
-as.data.frame(sce.all@assays$RNA@counts[1:10, 1:2])
-head(sce.all@meta.data, 10)
-table(sce.all@meta.data$orig.ident) 
-
-
 
 ###### step2:QC ######
 
-dir.create("./1-QC")
-setwd("./1-QC") 
-
+## calculate mitochondrial cells 
 mito_genes=rownames(sce.all)[grep("^MT-", rownames(sce.all))] 
-mito_genes 
+
 sce.all=PercentageFeatureSet(sce.all, "^MT-", col.name = "percent_mito")
 fivenum(sce.all@meta.data$percent_mito)
 
+## calculate ribosome cells
 ribo_genes=rownames(sce.all)[grep("^Rp[sl]", rownames(sce.all),ignore.case = T)]
-ribo_genes
+
 sce.all=PercentageFeatureSet(sce.all, "^RP[SL]", col.name = "percent_ribo")
 fivenum(sce.all@meta.data$percent_ribo)
-# 
+
+# calculate hemoglobin
 rownames(sce.all)[grep("^Hb[^(p)]", rownames(sce.all),ignore.case = T)]
 sce.all=PercentageFeatureSet(sce.all, "^HB[^(P)]", col.name = "percent_hb")
 fivenum(sce.all@meta.data$percent_hb)
-#
+
+# visualization 
 feats <- c("nFeature_RNA", "nCount_RNA", "percent_mito", "percent_ribo", "percent_hb")
 feats <- c("nFeature_RNA", "nCount_RNA")
 p1=VlnPlot(sce.all, group.by = "orig.ident", features = feats, pt.size = 0.01, ncol = 2) + 
   NoLegend()
-p1
-library(ggplot2) 
-ggsave(filename="Vlnplot1.png",plot=p1)
+
 feats <- c("percent_mito", "percent_ribo", "percent_hb")
 p2=VlnPlot(sce.all, group.by = "orig.ident", features = feats, pt.size = 0.01, ncol = 3, same.y.lims=T) + 
   scale_y_continuous(breaks=seq(0, 100, 5)) +
   NoLegend()
-p2	
-ggsave(filename="Vlnplot2.png",plot=p2)
 
 p3=FeatureScatter(sce.all, "nCount_RNA", "nFeature_RNA", group.by = "orig.ident", pt.size = 0.5)
-ggsave(filename="Scatterplot.png",plot=p3)
-#
-#
+
+# filter low-quality cells
+
 selected_c <- WhichCells(sce.all, expression = nFeature_RNA > 300)
 selected_f <- rownames(sce.all)[Matrix::rowSums(sce.all@assays$RNA@counts > 0 ) > 3]
 
@@ -86,7 +78,7 @@ dim(sce.all.filt)
 C=sce.all.filt@assays$RNA@counts
 dim(C)
 C=Matrix::t(Matrix::t(C)/Matrix::colSums(C)) * 100
-# 
+# random sampling (C too large)
 C=C[,sample(1:ncol(C),100)]
 most_expressed <- order(apply(C, 1, median), decreasing = T)[50:1]
 pdf("TOP50_most_expressed_gene.pdf",width=14)
@@ -98,7 +90,7 @@ boxplot(as.matrix(Matrix::t(C[most_expressed, ])),
 dev.off()
 rm(C)
 
-# cell cycle
+# scoring by cell cycle 
 sce.all.filt = NormalizeData(sce.all.filt)
 s.genes=Seurat::cc.genes.updated.2019$s.genes
 g2m.genes=Seurat::cc.genes.updated.2019$g2m.genes
@@ -108,12 +100,10 @@ sce.all.filt=CellCycleScoring(object = sce.all.filt,
                               set.ident = TRUE)
 p4=VlnPlot(sce.all.filt, features = c("S.Score", "G2M.Score"), group.by = "orig.ident", 
            ncol = 2, pt.size = 0.1)
-p4
 
-ggsave(filename="Vlnplot4_cycle.png",plot=p4)
 sce.all.filt@meta.data  %>% ggplot(aes(S.Score,G2M.Score))+geom_point(aes(color=Phase))+
   theme_minimal()
-ggsave(filename="cycle_details.pdf" )
+
 # high S.Score: S，high G2M.Score: G2M，both low: G1
 
 ###### step3: seurat ######
@@ -125,16 +115,13 @@ sce.all.filt = RunPCA(sce.all.filt, npcs = 20)
 sce.all.filt = RunTSNE(sce.all.filt, npcs = 20)
 sce.all.filt = RunUMAP(sce.all.filt, dims = 1:10)
 
-sce.all.filt
 sce.all.filt <- FindNeighbors(sce.all.filt, dims = 1:15)
 sce.all.filt <- FindClusters(sce.all.filt, resolution = 0.8)
-table(sce.all.filt@meta.data$RNA_snn_res.0.8)  
+# table(sce.all.filt@meta.data$RNA_snn_res.0.8)  
  
 DimPlot(sce.all.filt, 
         reduction = 'umap')
-ggsave('first_umap_by_seurat_cluster.pdf')
 
-library(ggplot2) 
 genes_to_check = c('PTPRC', 'CD3D', 'CD3E', 'CD4','CD8A','CD19', 'CD79A', 'MS4A1' ,
                    'IGHG1', 'MZB1', 'SDC1',
                    'CD68', 'CD163', 'CD14', 
@@ -143,17 +130,12 @@ genes_to_check = c('PTPRC', 'CD3D', 'CD3E', 'CD4','CD8A','CD19', 'CD79A', 'MS4A1
                    'FGF7','MME', 'ACTA2',
                    'PECAM1', 'VWF', 
                    'EPCAM' , 'KRT19', 'PROM1', 'ALDH1A1' )
-library(stringr)  
+
 p_all_markers <- DotPlot(sce.all.filt, features = genes_to_check,
                          assay='RNA'  )  + coord_flip()
 
-p_all_markers
-ggsave(plot=p_all_markers,
-       filename="first_check_all_marker_by_seurat_cluster.pdf",width = 12)
-
 
 ###### step4 ######
-
 
 sce.all=sce.all.filt
 colnames(sce.all@meta.data)
@@ -164,13 +146,11 @@ celltype[celltype$ClusterID %in% c(2),2]='myeloid'
 celltype[celltype$ClusterID %in% c(0 ),2]='epithelial' 
 celltype[celltype$ClusterID %in% c( 1),2]='fibo'   
 
-head(celltype)
-celltype 
-table(celltype$celltype)
 sce.all@meta.data$celltype = "NA"
 for(i in 1:nrow(celltype)){
   sce.all@meta.data[which(sce.all@meta.data$seurat_clusters == celltype$ClusterID[i]),'celltype'] <- celltype$celltype[i]}
-table(sce.all@meta.data$celltype)
+
+# table(sce.all@meta.data$celltype)
 
 genes_to_check = c('PTPRC', 'CD3D', 'CD3E', 'CD4','CD8A','CD19', 'CD79A', 'MS4A1' ,
                    'IGHG1', 'MZB1', 'SDC1',
@@ -183,20 +163,13 @@ genes_to_check = c('PTPRC', 'CD3D', 'CD3E', 'CD4','CD8A','CD19', 'CD79A', 'MS4A1
 p <- DotPlot(sce.all, features = genes_to_check,
              assay='RNA' ,group.by = 'celltype' )  + coord_flip()
 
-p
-ggsave(plot=p, filename="check_marker_by_celltype.pdf")
-table(sce.all@meta.data$celltype,sce.all@meta.data$seurat_clusters)
+# table(sce.all@meta.data$celltype,sce.all@meta.data$seurat_clusters)
 
 DimPlot(sce.all, reduction = "umap", group.by = "celltype",label = T)  
-ggsave('umap_by_celltype.pdf')
 
-library(patchwork)
 p_all_markers=DotPlot(sce.all, features = genes_to_check,
                       assay='RNA' ,group.by = 'celltype' )  + coord_flip()
 p_umap=DimPlot(sce.all, reduction = "umap", group.by = "celltype",label = T) 
-p_all_markers+p_umap
-ggsave('markers_umap_by_celltype.pdf',units = 'cm',
-       width = 22,height = 11)
 
 
 ###### step5 (auto cell annotation) ######
@@ -205,9 +178,7 @@ ggsave('markers_umap_by_celltype.pdf',units = 'cm',
 ###### step6 ######
 
 sce=sce.all
-sce
 Idents(sce)=sce@meta.data$seurat_clusters
-table(Idents(sce))  
 sce.markers <- FindAllMarkers(object = sce, only.pos = TRUE, min.pct = 0.25, 
                               thresh.use = 0.25)
 
@@ -217,7 +188,6 @@ write.csv(sce.markers,file=paste0(pro,'_sce.markers.csv'))
 library(dplyr) 
 top10 <- sce.markers %>% group_by(cluster) %>% top_n(10, avg_log2FC)
 DoHeatmap(sce,top10$gene,size=3)
-ggsave(filename=paste0(pro,'_sce.markers_heatmap.pdf'))
 
 table(sce$celltype,sample_ann$Biological_Condition)
 
