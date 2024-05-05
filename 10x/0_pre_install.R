170c6afd5d049c338e5b64e58a5acbb67153fc09
EvelynJ <50031451+jiangsizhu1201@users.noreply.github.com>
2024-05-05 16:00:23 -0400

    Add files via upload

---
+++ 10x/0_pre_install.R
@@ -0,0 +1,41 @@
+#options("repos"="https://mirrors.ustc.edu.cn/CRAN/")
+if(!require("BiocManager")) install.packages("BiocManager",update = F,ask = F)
+#options(BioC_mirror="https://mirrors.ustc.edu.cn/bioc/")
+
+cran_packages <- c('Seurat',
+                   'tidyverse'
+                   ) 
+Biocductor_packages <- c('sva',
+                         'monocle',
+                         'GOplot',
+                         'GSVA',
+                         'plotmo',
+                         'regplot',
+                         'scRNAseq',
+                         'BiocStyle',
+                         'celldex',
+                         'SingleR',
+                         'BiocParallel',
+                         'scater'
+)
+
+for (pkg in cran_packages){
+  if (! require(pkg,character.only=T) ) {
+    install.packages(pkg,ask = F,update = F)
+    require(pkg,character.only=T) 
+  }
+}
+
+
+for (pkg in Biocductor_packages){
+  if (! require(pkg,character.only=T) ) {
+    BiocManager::install(pkg,ask = F,update = F)
+    require(pkg,character.only=T) 
+  }
+}
+
+for (pkg in c(Biocductor_packages,cran_packages)){
+  require(pkg,character.only=T) 
+}
+
+
