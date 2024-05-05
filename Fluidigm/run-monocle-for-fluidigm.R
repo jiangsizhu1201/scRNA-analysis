e62ab94d9467204110da791df4ededb9fdf0f4cb
EvelynJ <50031451+jiangsizhu1201@users.noreply.github.com>
2024-05-05 15:05:43 -0400

    Update and rename run-monocle-for-fluidigm.R to Fluidigm/run-monocle-for-fluidigm.R

--- Fluidigm/run-monocle-for-fluidigm.R
+++ Fluidigm/run-monocle-for-fluidigm.R
@@ -10,12 +10,12 @@ library("FactoMineR")
 library("factoextra")
 library("ROCR")
 
-## ----- Load Example Data -----
+## ----- Example Data -----
 fluidigm <- ReprocessedFluidigmData()
-fluidigm
+# fluidigm
 
 ct <- floor(assays(fluidigm)$rsem_counts)
-ct[1:4,1:4] 
+# ct[1:4,1:4] 
 sample_ann <- as.data.frame(colData(fluidigm))
 DT::datatable(sample_ann)
 
@@ -23,16 +23,18 @@ gene_ann <- data.frame(
   gene_short_name = rownames( ct ) , 
   row.names =  rownames( ct ) 
 )
-head(gene_ann)
+# head(gene_ann)
 
 pd <- new("AnnotatedDataFrame",
           data=sample_ann)
 fd <- new("AnnotatedDataFrame",
           data=gene_ann) 
-ct[1:4,1:4]
+# ct[1:4,1:4]
 
 
 ###### step2: monocle ######
+
+# create object + basic dimension reduction
 
 sc_cds <- newCellDataSet(
   as.matrix(ct), 
@@ -40,22 +42,21 @@ sc_cds <- newCellDataSet(
   featureData =fd,
   expressionFamily = negbinomial.size(),
   lowerDetectionLimit=1)
-sc_cds
+sc_cds # CellDataSet 
 
-# CellDataSet 
+# standard monocle processing
 
-library(monocle)
-sc_cds
 sc_cds <- detectGenes(sc_cds, min_expr = 1) 
 sc_cds <- sc_cds[fData(sc_cds)$num_cells_expressed > 10, ]
-sc_cds
+# sc_cds
 
 cds <- sc_cds
 cds <- estimateSizeFactors(cds)
 cds <- estimateDispersions(cds) 
 # plyr 
 
-# select genes
+# select genes + find clusters
+
 disp_table <- dispersionTable(cds)
 unsup_clustering_genes <- subset(disp_table,
                                  mean_expression >= 0.1)
@@ -65,31 +66,33 @@ cds <- setOrderingFilter(cds,
 plot_ordering_genes(cds) 
 plot_pc_variance_explained(cds, 
                            return_all = F) # norm_method='log'
-# num_dim determined by PCA
-cds <- reduceDimension(cds, max_components = 2, num_dim = 6,
+
+cds <- reduceDimension(cds, max_components = 2, num_dim = 6, # num_dim determined by PCA 
                        reduction_method = 'tSNE', verbose = T)
 cds <- clusterCells(cds, num_clusters = 6) 
 plot_cell_clusters(cds, 1, 2 )
-table(pData(cds)$Cluster) 
-colnames(pData(cds)) 
-table(pData(cds)$Biological_Condition)
-table(pData(cds)$Cluster,pData(cds)$Biological_Condition) 
+# table(pData(cds)$Cluster) 
+# colnames(pData(cds)) 
+# table(pData(cds)$Biological_Condition)
+# table(pData(cds)$Cluster,pData(cds)$Biological_Condition) 
 plot_cell_clusters(cds, 1, 2 )
 
 save(cds,file = 'input_cds.Rdata')
 
-
-
 ###### step3: trajectory ######
 
-### monocle ### 
-cds 
+###### step3.1: monocle ######
+
+# check the trajectory for certain biological condition
 
 table(pData(cds)$Cluster)
 table(pData(cds)$Cluster,pData(cds)$Biological_Condition)
 plot_cell_clusters(cds, 1, 2 )
 
+# Note: use the seurat_clusters, not monocle results
+
 # Biological conditions
+
 pData(cds)$Cluster=pData(cds)$Biological_Condition
 table(pData(cds)$Cluster)
 
@@ -110,30 +113,33 @@ plot_genes_jitter(cds[cg,],
                   nrow= 3,
                   ncol = NULL )
 cg2=as.character(tail(sig_genes$gene_short_name)) 
+
+# Find the most significant genes for visualization
+
 plot_genes_jitter(cds[cg2,],
                   grouping = "Cluster",
                   color_by = "Cluster",
                   nrow= 3,
                   ncol = NULL )
 
+###### step3.2: trejactory ######
 
-
-# select gene set (significant)
-
+# step 1: select DEGs
 ordering_genes <- row.names (subset(diff_test_res, qval < 0.01))
 ordering_genes
 cds <- setOrderingFilter(cds, ordering_genes)
 plot_ordering_genes(cds) 
+# step 2: dimension reduction
 
 cds <- reduceDimension(cds, max_components = 2,
-                       method = 'DDRTree')
+                       method = 'DDRTree') # default by Monocle2
+# step 3: order cells
+cds <- orderCells(CDs)
 
-cds <- orderCells(cds)
-
+# step 4: visualization
 plot_cell_trajectory(cds, color_by = "Cluster")  
 ggsave('monocle_cell_trajectory_for_seurat.pdf')
 
-length(cg)
 plot_genes_in_pseudotime(cds[cg,],
                          color_by = "Cluster") 
 ggsave('monocle_plot_genes_in_pseudotime_for_seurat.pdf')
@@ -142,6 +148,8 @@ phe=pData(cds)
 boxplot(phe$Pseudotime,phe$Cluster)
 
 # https://davetang.org/muse/2017/10/01/getting-started-monocle/
+
+# step 4: find genes that change along the trajectory
 
 my_cds_subset=cds
 # pseudotime is now a column in the phenotypic data as well as the cell state
@@ -156,8 +164,7 @@ head(my_pseudotime_de)
 save( my_cds_subset,my_pseudotime_de,
       file = 'output_of_monocle.Rdata')
 
-
-###### step4 ######
+###### step4: Visualization of Monocle ######
 
 library(Seurat)
 library(gplots)
